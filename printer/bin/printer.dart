4d3930d37962892f412c0785f057399d1eb2f79c
Bartek Pacia <barpac02@gmail.com>
2023-05-11 02:44:06 +0200

    initial commit

---
+++ printer/bin/printer.dart
@@ -0,0 +1,5 @@
+import 'package:printer/printer.dart' as printer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${printer.calculate()}!');
+}
---
+++ printer/test/printer_test.dart
@@ -0,0 +1,8 @@
+import 'package:printer/printer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
